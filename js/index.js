1d2a2d4163f838fa178c2cde158e3b421f8b0834
Kipchirchir Albert <albertkipchirchir02@gmail.com>
2022-03-22 14:06:38 +0300

    potters wheel desktop accepted

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let home = document.querySelector(".home");
 let projects = document.querySelector(".projects");
+let main = document.querySelector("main");
 projects.classList.add("hide");
 
 document.querySelectorAll(".nav-link").forEach((navLink) =>
@@ -10,12 +11,23 @@ document.querySelectorAll(".nav-link").forEach((navLink) =>
       home.classList.remove("slideout");
       home.classList.add("slidein");
       projects.classList.add("slideout");
+      setTimeout(() => {
+        projects.classList.add("hide");
+        main.style.height = "75vh";
+      }, 1000);
     } else if (target === "projects") {
       home.classList.remove("slidein");
       home.classList.add("slideout");
       projects.classList.remove("hide");
+      // console.log(projects.clientHeight);
       projects.classList.remove("slideout");
       projects.classList.add("slidein");
+
+      main.style.height = "800px";
+      setTimeout(() => {
+        home.classList.add("hide");
+        console.log(e.target.clientHeight);
+      }, 1000);
     } else {
       alert("no such element");
     }
